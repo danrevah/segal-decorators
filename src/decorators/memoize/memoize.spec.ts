da0a1083e37c7ac483d6b5730fa216a28b5e5a66
Dan Revah <danrevah89@gmail.com>
2019-02-17 14:48:46 +0300

    feat(memoize): add memoize decorator

--- src/decorators/memoize/memoize.spec.ts
+++ src/decorators/memoize/memoize.spec.ts
@@ -1,12 +1,27 @@
 import { expect } from 'chai';
+import * as chai from 'chai';
+import * as spies from 'chai-spies';
 import 'mocha';
 import { Memoize } from './memoize';
+
+chai.use(spies);
+
+function hash() {
+  return 'hash';
+}
+
+const spyHash = chai.spy(hash);
 
 class TestMemoize {
   counter = 0;
 
   @Memoize()
-  count() {
+  count(...args: any[]) {
+    return ++this.counter;
+  }
+
+  @Memoize(spyHash)
+  countHash(...args: any[]) {
     return ++this.counter;
   }
 }
@@ -14,8 +29,23 @@ class TestMemoize {
 describe('Memoize', () => {
   it('should memoize values', () => {
     const test = new TestMemoize();
+    expect(test.count()).to.equal(1);
+    expect(test.count()).to.equal(1);
+  });
 
-    expect(test.count()).to.equal(1);
-    expect(test.count()).to.equal(1);
+  it('should not memoize different calls', () => {
+    const test = new TestMemoize();
+    expect(test.count(1)).to.equal(1);
+    expect(test.count(1)).to.equal(1);
+    expect(test.count(2)).to.equal(2);
+    expect(test.count(2)).to.equal(2);
+    expect(test.count(1, 2)).to.equal(3);
+  });
+
+  it('should work call hash function', () => {
+    const test = new TestMemoize();
+    expect(spyHash).to.have.not.been.called();
+    test.countHash();
+    expect(spyHash).to.have.been.called();
   });
 });
